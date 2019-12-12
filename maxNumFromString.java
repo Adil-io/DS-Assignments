9073e405f9186bc37fbf478fe332ced776c48d0b
ADIL YUSUF <55436939+Adil-io@users.noreply.github.com>
2019-12-12 18:25:18 +0530

    Form Largest Number from Array of Numbers

---
+++ maxNumFromString.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class maxNumFromString{
+	static long maxNum(String s){
+		String[] str=s.split(" ");
+		int n=str.length;
+		Arrays.sort(str,Collections.reverseOrder());
+		String num="";
+		for(int i=0;i<n;i++){
+			num=num+str[i];
+		}
+		long max=Long.parseLong(num);
+		return max;	
+	}
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args){
+		System.out.println("Enter Array of Numbers :-");
+		String s=sc.nextLine();
+		System.out.println("Largest Number formed is :-\n"+maxNum(s));
+	}
+}
