04a8fed91268f05bab96d66ac90f5d52079cdab8
ADIL YUSUF <55436939+Adil-io@users.noreply.github.com>
2020-01-31 22:50:00 +0530

    Update patternV.java

--- patternV.java
+++ patternV.java
@@ -6,18 +6,18 @@ public class patternV{
 		for(int i=0;i<n;i++){
 			for(int j=n;j>0;j--){
 				if(j<=i){
-					System.out.print("  ");
+					System.out.print(" ");
 				}
 				else{
-					System.out.print(j+" ");
+					System.out.print(j);
 				}					
 			}
 			for(int j=2;j<=n;j++){
 				if(j<=i){
-					System.out.print("  ");
+					System.out.print(" ");
 				}
 				else{
-					System.out.print(j+" ");
+					System.out.print(j);
 				}	
 			}
 			System.out.println();
